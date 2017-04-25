d06a52bdd4c5eaf88b5aa9adb12fba111f575766
Richard Hertzog <richard.hertzog@gmail.com>
2017-04-24 22:34:13 -0700

    basic skeleton

---
+++ client/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hi guyz
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
