e5fc552fd752aa6bb4a76a22450e93bf98cfdc2d
Richard Hertzog <richard.hertzog@gmail.com>
2017-05-02 20:56:50 -0700

    mongodb

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const db = require('db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use('/bundles', express.static(path.join(__dirname, '../bundles')));
+app.use(express.static(path.join(__dirname, '../bundles')));
 
 app.listen(PORT, () => {
   console.log('server running on port', PORT);
