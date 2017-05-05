01792672269e163f6c317fb152501d7d257d6945
Richard Hertzog <richard.hertzog@gmail.com>
2017-05-05 00:10:35 -0700

    will retrieve parking spots

--- server/controllers/parkingSpotController.js
+++ server/controllers/parkingSpotController.js
@@ -5,5 +5,11 @@ exports.createParkingSpot = (req, res) => {
   const parkingSpot = new ParkingSpot({ lat, lng, paid });
   parkingSpot.save()
     .then(response => res.status(200).send(response))
-    .catch(() => res.status(422).send({ error: 'could not save parkingSpot' }));
+    .catch(() => res.status(422).send({ error: 'could not save parking spot' }));
+};
+
+exports.getParkingSpots = (req, res) => {
+  ParkingSpot.find()
+    .then(parkingSpots => res.status(200).send(parkingSpots))
+    .catch(() => res.status(422).send({ error: 'could not get parking spots' }));
 };
