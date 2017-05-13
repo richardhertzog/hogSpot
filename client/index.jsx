2ca898a3b47593c870356828bdfed5c81159bd05
Richard Hertzog <richard.hertzog@gmail.com>
2017-05-12 20:25:40 -0700

    setup react router

--- client/index.jsx
+++ client/index.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxPromise from 'redux-promise';
+import { IndexRoute, Router, Route, hashHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        hi guyz
-      </div>
-    );
-  }
-}
+import App from './components/App.jsx';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const history = syncHistoryWithStore(hashHistory, store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={App} >
+        <IndexRoute component={Landing} />
+        <Route path="home" component={Home} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('app'),
+);
