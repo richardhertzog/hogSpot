8522d55bccf0a1438e91f660347ea0fc8d5c7d56
Richard Hertzog <richard.hertzog@gmail.com>
2026-04-20 17:19:03 -0700

    add mapbox

--- client/main.js
+++ client/main.js
@@ -1,1 +1,10 @@
-console.log("hogSpot loaded");
+import { MAPBOX_TOKEN } from "./config.js";
+
+mapboxgl.accessToken = MAPBOX_TOKEN;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: [-122.4194, 37.7749], // San Francisco
+  zoom: 13,
+});
